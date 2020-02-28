e171225b02b318ceb08547c7b492c0e9f0de9bfa
pixelboogie <justamovie@yahoo.com>
2020-02-28 09:05:53 -0800

    Added Conveyer Belt

---
+++ ConveyerBelt.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyerBelt : MonoBehaviour
+{
+
+
+public GameObject belt;
+public Transform endpoint;
+public float speed;
+
+private void Update() {
+            // Debug.Log("_______________________________Update");
+}
+
+
+void OnTriggerStay(Collider other){
+//  private void OnCollisionStay(Collision other) {
+
+
+      // Debug.Log("_______________________________TRIGGER");
+      other.transform.position = Vector3.MoveTowards(other.transform.position, endpoint.position, speed * Time.deltaTime);
+}
+
+}
