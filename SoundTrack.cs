a1312231c3d84759fd009a7d7747ee274202fa75
pixelboogie <justamovie@yahoo.com>
2020-02-28 16:12:14 -0800

    Added toggle to soundtrack; Commented Waves

---
+++ SoundTrack.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundTrack : MonoBehaviour
+{
+
+      AudioSource m_AudioSource;
+       public static bool musicOn = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_AudioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+          if (OVRInput.GetDown(OVRInput.RawButton.Y)){
+      //     if (OVRInput.GetDown(OVRInput.Button.Four)){
+      //     if (OVRInput.GetDown(OVRInput.Touch.Two)){
+                if(musicOn){
+                  //     Debug.Log("----------------- stop it");
+                      musicOn = false;
+                        m_AudioSource.Stop();
+                }else{
+                        //  Debug.Log("----------------- play it");
+                      musicOn = true;
+                      m_AudioSource.Play();
+                }
+          }
+    }
+}
--- Waves.cs
+++ Waves.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 
 public class Waves : MonoBehaviour
 {
-    public float difficulty = 0.4f;
-    public float difficultyIncreaseSpeed = 0.01f;
+    public float difficulty = 0.4f; // used in 2 formulas: speed and health(color)
+    public float difficultyIncreaseSpeed = 0.01f; // how much is added each update to difficulty
     public Transform startPosition;
     public GameObject ballonGreen;
 
     //Timer
-    private float ballonTimer = 0f;
-    private float nextBallon = 1f;
+    private float ballonTimer = 0f; // so they can spawn every frame???
+    private float nextBallon = 1f; // amt of time to wait before spawning next???
 
     //Ballon limiter
-    public int ballonsPerWave = 5;
-    private int ballonsCount = 1;
+    public int ballonsPerWave = 5;  // how many to spawn per wave???
+    private int ballonsCount = 1; // number of balloons spawned
     //waves timer
-    public float wavesTimer = 0f;
-    public float nextWave = 20f;
+    public float wavesTimer = 0f; // set to nextWave + currenttime
+    public float nextWave = 20f; // amount of time to wait before the next way
 
     // Update is called once per frame
     void Update()
