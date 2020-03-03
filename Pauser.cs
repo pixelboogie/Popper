84765bcbc39aba6269c743a3464362ab4d8a961b
pixelboogie <justamovie@yahoo.com>
2020-03-03 11:14:11 -0800

    Make game pause when app loses focus (HMD Removed)

---
+++ Pauser.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pauser : MonoBehaviour
+{
+
+      GameObject referenceObject;
+      SoundTrack referenceScript;
+
+      private float waitTime = 5f;
+
+      private bool gameIsPaused = false;
+
+
+      // Start is called before the first frame update
+      void Start()
+      {
+            referenceObject = GameObject.FindWithTag("Soundtrack");
+            referenceScript = referenceObject.GetComponent<SoundTrack>();
+      }
+
+      // Update is called once per frame
+      void Update()
+      {
+
+            waitTime -= Time.deltaTime;
+
+            if (waitTime < 0)
+            {
+                  checkFocus();
+            }
+
+
+      }
+
+
+
+      void checkFocus()
+      {
+            if (OVRManager.hasVrFocus == false)
+            {
+                  gameIsPaused = true;
+                  pauseIt();
+
+            }
+            else
+            {
+                  //  gameIsPaused = false;
+                  resumeIt();
+            }
+      }
+
+      void pauseIt()
+      {
+
+            Debug.Log("----------------- stop it");
+
+            // pauseMenuUI.SetActive(true);
+            // gameIsPaused = true;
+
+
+            // referenceObject = GameObject.FindWithTag("ObjectOne");
+            // referenceScript = referenceObject.GetComponent<Gun>();
+            // referenceScript.SetActive(true);
+            if (GameVariables.musicOn)
+            {
+                  referenceScript.PauseMusic();
+            }
+            Time.timeScale = 0f;
+      }
+
+      void resumeIt()
+      {
+
+            if(gameIsPaused){
+
+            Debug.Log("----------------- play it");
+            waitTime = 5f;
+            // pauseMenuUI.SetActive(false);
+            gameIsPaused = false;
+
+            if (GameVariables.musicOn)
+            {
+            referenceScript.UnPauseMusic();
+            }
+            Time.timeScale = 1f;
+            // referenceObject = GameObject.FindWithTag("ObjectOne");
+            // referenceScript = referenceObject.GetComponent<Gun>();
+            // referenceScript.SetActive(false);
+            }
+      }
+}
--- SoundTrack.cs
+++ SoundTrack.cs
@@ -7,34 +7,57 @@ public class SoundTrack : MonoBehaviour
 
       AudioSource m_AudioSource;
       //  public static bool musicOn = true;
-      
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        m_AudioSource = GetComponent<AudioSource>();
-           if(GameVariables.musicOn){
-             m_AudioSource.Play();
-           }
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
+      // Start is called before the first frame update
+      void Start()
+      {
+            m_AudioSource = GetComponent<AudioSource>();
+            if (GameVariables.musicOn)
+            {
+                  m_AudioSource.Play();
+            }
+      }
 
-          if (OVRInput.GetDown(OVRInput.RawButton.Y)){
-      //     if (OVRInput.GetDown(OVRInput.Button.Four)){
-      //     if (OVRInput.GetDown(OVRInput.Touch.Two)){
-                if(GameVariables.musicOn){
+      // Update is called once per frame
+      void Update()
+      {
+
+            if (OVRInput.GetDown(OVRInput.RawButton.Y))
+            {
+                  //     if (OVRInput.GetDown(OVRInput.Button.Four)){
+                  //     if (OVRInput.GetDown(OVRInput.Touch.Two)){
+                  ToggleMusic();
+            }
+      }
+
+
+
+      void ToggleMusic()
+      {
+            if (GameVariables.musicOn)
+            {
                   //     Debug.Log("----------------- stop it");
-                      GameVariables.musicOn = false;
-                        m_AudioSource.Stop();
-                }else{
-                        //  Debug.Log("----------------- play it");
-                      GameVariables.musicOn = true;
-                      m_AudioSource.Play();
-                }
-          }
-    }
+                  GameVariables.musicOn = false;
+                  m_AudioSource.Stop();
+            }
+            else
+            {
+                  //  Debug.Log("----------------- play it");
+                  GameVariables.musicOn = true;
+                  m_AudioSource.Play();
+            }
+      }
+
+
+      public void PauseMusic()
+      {
+            m_AudioSource.Stop();
+      }
+
+      public void UnPauseMusic()
+      {
+            m_AudioSource.Play();
+      }
 }
